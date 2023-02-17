297aa2f9d164925ea93476608335cc6447950dc9
pubest <pubestpubest@gmail.com>
2023-02-17 16:22:35 +0700

    lab19 complete

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -20,20 +20,62 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
-
+void importDataFromFile(string filename,vector<string> &names, vector<int> &score, vector<char> &grade){
+    ifstream source(filename.c_str());
+    string cpps; 
+    char format[]="%[^:]: %d %d %d";
+    while(getline(source,cpps)){
+        const char * cs = cpps.c_str();
+        int scores[3]={0,0,0};
+        char name[40];
+        sscanf(cs,format,name,&scores[0],&scores[1],&scores[2]);
+        names.push_back(name);
+        score.push_back(scores[0]+scores[1]+scores[2]);
+        grade.push_back(score2grade(scores[0]+scores[1]+scores[2]));
+    }
 }
 
-void getCommand(){
-
+void getCommand(string &comm,string &key){
+    string txt;
+    cout<<"Please input your command: ";
+    getline(cin,txt);
+    int start=0;
+    int end = txt.find_first_of(" ");
+    comm=txt.substr(start,end-start);
+    start=end+1;
+    key=txt.substr(start);
 }
 
-void searchName(){
-
+void searchName(vector<string> names,vector<int> score,vector<char> grade,string key){
+    unsigned i=0;
+    bool found=false;
+    cout << "---------------------------------\n";
+    while(i<names.size()){
+        if(toUpperStr(names.at(i))==toUpperStr(key)){
+            found=true;
+            cout<<names.at(i)<<"'s score = "<<score.at(i)<<endl;
+            cout<<names.at(i)<<"'s grade = "<<grade.at(i)<<endl;
+        }
+        i++;
+    }
+    if(!found)  printf("Cannot found.\n");
+    cout << "---------------------------------\n";
 }
 
-void searchGrade(){
-
+void searchGrade(vector<string> names,vector<int> score,vector<char> grade,string key){
+    unsigned i=0;
+    bool found=false;
+    cout << "---------------------------------\n";
+    while(i<names.size()){
+        string g(1,grade.at(i));
+        if(toUpperStr(g)==toUpperStr(key)){
+            found = true;
+            cout<<names.at(i)<<" ("<<score.at(i)<<")\n";
+        }
+        i++;
+    }
+    if(!found)  printf("Cannot found.\n");
+    cout << "---------------------------------\n";
 }
 
 
